954cfa0a811814b0113c6dafe2f8c0a8348707c0
Mike Merin <mikemerinweather@gmail.com>
2017-03-14 19:43:03 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,1 +1,11 @@
 def roll() rand(6) + 1 end
+
+=begin
+
+# also works, and can be changed to work with other ranges like letters:
+
+def roll() (1..6).to_a[rand(6)] end
+# letters:
+# def roll() ("a".."z").to_a[rand(26)] end
+
+=end
