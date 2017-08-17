0d3252ba7234ae8ce8f6820d0f49adf94d58cc85
gene-yoo <gene.y.yoo@gmail.com>
2017-08-17 10:49:13 -0400

    Lab complete.

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,12 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  # code goes here
+  return rand(1..6)
 end
+
+=begin
+def roll
+  dice = [1, 2, 3, 4, 5, 6]
+  return dice(rand(6))
+end
+=end
