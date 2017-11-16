ee8566c5d1022d03c43bfabd308b9bfb90a5f3df
TorreJohnson <torre.johnson@gmail.com>
2017-11-16 00:36:46 -0500

    defined #roll with two solutions

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,12 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+#def roll
+#  dice = rand(1..6)
+#  dice
+#end
+
 def roll
-  # code goes here
+  dice = [1, 2, 3, 4, 5, 6]
+  dice_roll = dice[rand(dice.length)]
 end
