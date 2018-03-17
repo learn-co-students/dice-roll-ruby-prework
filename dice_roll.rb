3e80d00af2d91047041cb0282fb17182d7b369e4
github username <github email address>
2018-03-17 10:59:30 -0400

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,16 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+# def roll
+#   rand 1..6
+# end
+
+
+# BONUS
+# There are different ways to complete this lab.
+# First, try to solve it by using ranges. Then for some extra fun,
+# try to solve this lab using an array.
+
 def roll
-  # code goes here
+  [1,2,3,4,5,6].sample
 end
