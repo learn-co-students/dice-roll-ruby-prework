4d48405fab331e5652a5ef7ec38c9d09e1629e03
Aisha D'Souza <aishad.souza@yahoo.com>
2018-08-26 16:50:48 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,13 +2,13 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
-# def roll
-#   rand(1..6)
-# end
-
-
-dice = [1,2,3,,4,5,6]
-  
 def roll
-  dice.rand
+  rand(1..6)
+end
+
+#Then for some extra fun, try to solve this lab using an array.
+
+def roll
+dice = [1,2,3,4,5,6]
+ dice.sample
 end
