14289ff57c9a6b01894aa6de9e022e3acbae1dff
Bakhta Elamar <elamarb@icloud.com>
2018-09-30 02:00:38 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,7 +1,8 @@
 # Create method `roll` that returns a random number between 1 and 6
 
-# Feel free to google "how to generate a random number in ruby"
+# Feel free to google "how to generate a random number in rub
 
-def roll
-  # code goes here
+def roll(number) 
+ number = Math.floor(rand 6 + 1)
 end
+roll(number)
