07821ac205a501e7def4f3733bd72a1bf559f554
github username <github email address>
2019-01-31 09:40:45 -0500

    updated dice roll method

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,7 +2,13 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+#first way that I solved it
 def roll
   a = [1, 2, 3, 4, 5, 6]
   a.sample
+end
+
+#shorter solution
+def roll
+  rand(1..6)
 end
